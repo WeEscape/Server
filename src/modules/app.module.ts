cb90d270e05c5e75434e7012ac51539dcfd96e9c
JoEunsoo <dcpg95@gmail.com>
2022-10-19 23:52:32 +0900

    CHORE : 서버 초기세팅

--- src/modules/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UserModule } from './user.module';
-import { TaskModule } from './task.module';
-
-@Module({
-  imports: [UserModule, TaskModule],
-})
-export class AppModule {}
