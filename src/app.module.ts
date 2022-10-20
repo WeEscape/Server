e3e4e780a594c0a9115d951d7d86662fd8829b09
JoEunsoo <dcpg95@gmail.com>
2022-10-20 23:12:03 +0900

    CHORE : 초기세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { TaskModule } from './task/task.module';
+import { AppService } from './app.service';
 
 @Module({
   imports: [UserModule, TaskModule],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function dobbyServer() {
   const app = await NestFactory.create(AppModule);
-
+  const logger = new Logger();
+  const port = 8080;
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -13,6 +14,12 @@ async function dobbyServer() {
     }),
   );
 
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'HEAD', 'PUT', 'POST', 'DELETE'],
+    credentials: true,
+  });
   await app.listen(8080);
+  logger.log(`server is running, on port ${port}`);
 }
 dobbyServer();
