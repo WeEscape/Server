7f9783965e2f465e8170294da5598c5da6bf0588
JoEunsoo <dcpg95@gmail.com>
2022-10-27 21:13:21 +0900

    CHORE : 로그인 컨트롤러 추가

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { TaskModule } from './task/task.module';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, TaskModule],
+  imports: [UserModule, TaskModule, AuthModule],
   providers: [AppService],
 })
 export class AppModule {}
