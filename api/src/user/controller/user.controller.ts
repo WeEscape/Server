b4a46b4471a63b665ccf15e30b3d6a0463d747b3
JoEunsoo <dcpg95@gmail.com>
2022-10-28 22:38:51 +0900

    FEAT : typeorm 으로 Db 구현 로직

--- api/src/user/controller/user.controller.ts
+++ api/src/user/controller/user.controller.ts
@@ -1,9 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { UserService } from './../service/user.service';
+import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { AuthService } from 'src/auth/auth.service';
 
 @Controller('user')
 export class UserController {
+  // constructor() {} // private userService: UserService, // private readonly authService: AuthService,
   @Get()
   sayHellp() {
     return 'hello';
   }
+
+  // @Get(':email')
+  // async getUsers(@Res() res: Response, @Param() param) {
+  //   const user = await this.userService.getUser(param.email);
+  //   res.json(user);
+  // }
+
+  // @Post()
+  // async createUser(@Res() res: Response, @Body() body) {
+  //   await this.userService.createUser(body);
+  //   res.json('회원가입 완료!');
+  // }
 }
