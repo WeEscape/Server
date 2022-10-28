11910d82658a196a84ef79dfbb040b4663829923
JoEunsoo <dcpg95@gmail.com>
2022-10-28 22:39:42 +0900

    FEAT : jwt 로그인 로직

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -6,24 +6,43 @@ import {
   Get,
   Post,
   Query,
-  Response,
+  Res,
 } from '@nestjs/common';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authServive: AuthService) {}
+  constructor(private authService: AuthService) {}
   @Get()
   getAll() {
     return 'hellowolrd';
   }
-
-  @Get('/callback/kakao')
-  async login(@Query() query: any, @Response() res): Promise<any> {
-    try {
-      if (!query) return res.send('로그인 정보가 없습니다.').status(403);
-      const social = await this.authServive.kakaologin(query.code);
-    } catch (err) {
-      res.send(err);
-    }
+  // @Get('/callback/kakao')
+  // async login(@Query() query: any, @Response() res): Promise<any> {
+  //   try {
+  //     if (!query) return res.send('로그인 정보가 없습니다.').status(403);
+  //     const social = await this.authService.kakaoLogin(query.code);
+  //   } catch (err) {
+  //     res.send(err);
+  //   }
+  // }
+  @Post()
+  // async logins(@Body() data: any, @Res() res: string): Promise<any> {
+  //   const { access_token, social_type } = data;
+  //   try {
+  //     let profile: any;
+  //     if (social_type === 'kakao')
+  //       profile = await this.authService.kakao(access_token);
+  //     // const userInfo = await this.userService.getUser(profile.email);
+  //     // userInfo ? userInfo : await this.userService.createUser(profile);
+  //   } catch (err) {
+  //     return err;
+  //   }
+  // }
+  async login(@Res() res: Response, @Body() data) {
+    const userSearch = await this.authService.getUser(data.email);
+    if (!userSearch) return res.send('회원 정보가 없습니다.');
+    const success = await this.authService.logintest(data.email);
+    return res.send(success);
   }
 }
--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,9 +1,18 @@
+import { Users } from 'src/user/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+import { JwtService } from '@nestjs/jwt';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
-  async kakaologin(code: string): Promise<any> {
+  constructor(
+    private readonly jwtService: JwtService,
+    @InjectRepository(Users)
+    private usersRepository: Repository<Users>,
+  ) {}
+  async kakaoLogin(code: string): Promise<any> {
     const redir = 'http://localhost:8080/auth/callback/kakao';
     const kakaourl = `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${process.env.KAKAO_KEY}&redirect_uri=${redir}&code=${code}`;
 
@@ -16,6 +25,52 @@ export class AuthService {
       const { nickname, profile_image_url } =
         userInfo.data.kakao_account.profile;
       const { email } = userInfo.data.kakao_account;
+    } catch (err) {
+      return err;
+    }
+  }
+
+  // 토큰 발급 로직
+  async createToken(email: any): Promise<any> {
+    const payload = { email };
+    const accessToken = await this.jwtService.sign(payload);
+    const refreshToken = await this.jwtService.sign(
+      {},
+      {
+        expiresIn: '14d',
+      },
+    );
+    return { accessToken, refreshToken };
+  }
+
+  async getUser(email: string) {
+    const user = await this.usersRepository.findOne({
+      where: { email },
+    });
+    return user;
+  }
+
+  // 카카오 로그인 로직
+  async kakao(token: string): Promise<any> {
+    const kakaourl = 'https://kapi.kakao.com/v2/user/me';
+    try {
+      const userInfo = await axios.get(kakaourl, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      const { nickname: name, profile_image_url: profile_url } =
+        userInfo.data.kakao_account.profile;
+      const { email } = userInfo.data.kakao_account;
+      const accesstoken = await this.createToken(email);
+      return { name, profile_url, email, accesstoken };
+    } catch (err) {
+      return err;
+    }
+  }
+
+  // 로그인 토큰 테스트 로직
+  async logintest(data: string) {
+    try {
+      return await this.createToken(data);
     } catch (err) {
       return err;
     }
