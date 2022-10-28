2ce2dfc1c20b4e943c8267ae9d9c3d468759a3fc
JoEunsoo <dcpg95@gmail.com>
2022-10-28 22:36:38 +0900

    FEAT : mysql 연동 환경설정

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,9 +3,16 @@ import { UserModule } from './user/user.module';
 import { TaskModule } from './task/task.module';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as ormconfig from './ormconfig';
 
 @Module({
-  imports: [UserModule, TaskModule, AuthModule],
+  imports: [
+    UserModule,
+    TaskModule,
+    AuthModule,
+    TypeOrmModule.forRoot(ormconfig),
+  ],
   providers: [AppService],
 })
 export class AppModule {}
